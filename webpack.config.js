b4420e68ddaa49da5356f73c62c00e8b60875a5c
andre <andre4130@gmail.com>
2020-09-27 00:19:47 +0200

    updated files

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,30 @@
 const webpack = require('webpack');
 
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+
 module.exports = {
+  mode: 'development',
+  resolve: {
+      extensions: ['.js', '.jsx'],
+      alias: {
+          '@': path.resolve(__dirname, 'src/'),
+      }
+  },
+  plugins: [new HtmlWebpackPlugin({
+      template: './src/index.html'
+  })],
+  devServer: {
+      historyApiFallback: true
+  },
+  externals: {
+      // global app config object
+      config: JSON.stringify({
+          apiUrl: 'http://localhost:4000'
+      })
+  },
   entry: './src/index.js',
   module: {
     rules: [
@@ -13,7 +37,10 @@ module.exports = {
             options: {
               name: 'images/[hash]-[name].[ext]',
             },
-          },
+          },{
+            test: /\.jsx?$/,
+            loader: 'babel-loader'
+        }
         ],
       },
     ],
